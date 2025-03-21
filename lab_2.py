2ca6ce934333956ff724b6a8bbbe6054fe367821
Kate <134648812+rudenkoekatiae@users.noreply.github.com>
2025-03-21 12:55:58 +0200

    Add files via upload
    initializing merge sort instead of buikt in method

--- lab_2.py
+++ lab_2.py
@@ -17,7 +17,7 @@ def can_place_cows(stalls, cows, min_dist):
     return False
 
 def largest_min_distance(N, C, stalls):
-    stalls.sort()
+
     low, high = 2, stalls[-1] - stalls[0]
     best = 2
     global iteration
@@ -52,7 +52,34 @@ def generate_cow_names(C):
 
 N = 10 ** 5
 C = random.randint(1, 10 ** 3)
-free_sections = sorted(random.sample(range(1, N+1), N))
+
+def sorting(lst):
+    if len(lst) <= 1:
+        return lst
+
+    mid = len(lst) // 2
+    left_half = sorting(lst[:mid])
+    right_half = sorting(lst[mid:])
+
+    return merge(left_half, right_half)
+
+def merge(left, right):
+    sorted_lst = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            sorted_lst.append(left[i])
+            i += 1
+        else:
+            sorted_lst.append(right[j])
+            j += 1
+
+    sorted_lst.extend(left[i:])
+    sorted_lst.extend(right[j:])
+    return sorted_lst
+
+free_sections = sorting(random.sample(range(1, N+1), N))
 
 the_best_distance, iteration = largest_min_distance(N, C, free_sections)
 placed_positions = place_cows(free_sections, C, the_best_distance)
@@ -75,3 +102,9 @@ while True:
 
 for i, (position, name) in enumerate(placed_cows[:num_cows], 1):
     print(f"{i} cow: {name} in stall {position}")
+
+with open("cows.py", "w", encoding="utf-8") as f:
+    f.write("cows_data = [\n")
+    for position, name in placed_cows:
+        f.write(f"    ('{name}', {position}),\n")
+    f.write("]\n")
